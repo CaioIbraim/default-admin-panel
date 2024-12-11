5344eb9fbb54dad901c81c4a719ae5238d3f973b
Caio Ibraim <caiofabiocosta@gmail.com>
2024-12-11 00:07:47 +0000

    Correção das páginas do site!

---
+++ src/app/site/chatbot/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import  Nav  from "@/components/Nav";
+import  HeroSection  from "@/components/HeroSection";
+export default function Home() {
+  return (
+    <>
+     <Nav/>
+    <HeroSection/>
+    </>
+  );
+}
