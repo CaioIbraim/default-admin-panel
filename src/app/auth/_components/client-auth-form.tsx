bf83ebb63f6739efd40352bbb69c87e5d87a3666
Caio Ibraim <caiofabiocosta@gmail.com>
2024-12-11 11:11:29 +0000

    Correção no layout para cadastro e login de usuário

--- src/app/auth/_components/client-auth-form.tsx
+++ src/app/auth/_components/client-auth-form.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { toast } from "@/components/ui/use-toast"
+import { toast } from "@/hooks/use-toast"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Facebook, Loader2, Mail } from 'lucide-react'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
